fa7555a806168630ef3cbc004bf5ba22bc7ebad0
S <2055557@cognizant.com>
2025-10-08 15:32:01 +0530

    filter and stickysocialbar blocks added

---
+++ blocks/filter/filter.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.querySelector(".filter ul");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+  
+    // Create a new input element
+    const inputElement = document.createElement('input');
+  
+    // Set attributes for the input element
+    inputElement.setAttribute('type', 'text');
+    inputElement.setAttribute('id', 'myInput');
+    inputElement.setAttribute('placeholder', 'Search for names..');
+  
+    // Find an existing element in the DOM where you want to append the input
+    // For example, append it to the body or a specific div
+    const targetElement = document.querySelector('.filter');
+  
+    // Append the newly created input element to the target element
+    targetElement.prepend(inputElement);
+  inputElement.addEventListener('keyup', myFunction);
+    console.log('Input element created and appended to the DOM.');
---
+++ blocks/stickysocialbar/stickysocialbar.js
@@ -0,0 +1,16 @@
+const elem = document.querySelectorAll('.stickysocialbar ul li');
+
+
+
+ // Loop through all list items, and hide those who don't match the search query
+  for (var i = 0; i < elem.length; i++) {
+   let  a = elem[i].getElementsByTagName("a")[0];
+   let txtValue = a.textContent || a.innerText;
+     // Create a new input element
+  const inputElement = document.createElement('img');
+    inputElement.setAttribute('src', '/icons/'+txtValue+'.svg');
+      a.innerText = ''; 
+    a.textContent = '';
+ a.append(inputElement);
+  
+  }
